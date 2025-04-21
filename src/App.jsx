ca8f8abd3fc8cd968a43e7d738b68f3577828da4
nicooxlyy <junicoo2506@gmail.com>
2025-04-21 14:56:52 +0700

    Tugas Praktikum 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Greeting from './components/Greeting'; 
 import ProductList from './components/ProductList';
+import Mahasiswa from './components/Mahasiswa';
+import PostList from './components/PostList';
 
 
 
@@ -9,6 +11,8 @@ function App() {
       <h1>React Frontend - Pertemuan 9</h1> 
       <Greeting name="Mahasiswa" /> 
       <ProductList /> 
+      <Mahasiswa nama="Junico Baan" npm="3124522012" prodi="Teknik Informatika" /> 
+      <PostList />
     </div> 
   ); 
 }
